5732bd6243b7ded90afdad6efcf8565e5c9c6a2a
Aleksandr Filatov <greybax@gmail.com>
2016-04-23 19:59:21 -0700

    fixed #1

--- app/index.js
+++ app/index.js
@@ -24,8 +24,11 @@ module.exports = yeoman.Base.extend({
         this.argument('rulesDirectory', { type: Array, required: false,
             desc: 'rulesDirectory list: "yo tslint tslint-microsoft-contrib"'
         });
-        this.option('rulesDirectory', { type: String, required: false,
-            desc: 'Rules list: "yo tslint tslint-microsoft-contrib"'
+        this.option('rulesDirectory', { type: String, required: false, alias: "r",
+            desc: 'rulesDirectory list: "yo tslint -r tslint-microsoft-contrib"'
+        });
+        this.option('extends', { type: String, required: false, alias: "e",
+            desc: 'extends list: "yo tslint -e tslint-microsoft-contrib"'
         });
 
         this.saveDepsToPkg = function (deps) {
@@ -48,6 +51,7 @@ module.exports = yeoman.Base.extend({
             "codelyzer": "node_modules/codelyzer/dist/src"
         }
 
+        // arguments
         if (this.rulesDirectory) {
             cli.rulesDirectory = (typeof this.rulesDirectory === 'string')
                 ? splitKeywords(this.rulesDirectory)
@@ -61,24 +65,43 @@ module.exports = yeoman.Base.extend({
             );
         }
 
-        var options = this.options.config || {};
-        if (options.rulesDirectory) {
-            options.rulesDirectory = (typeof options.rulesDirectory === 'string')
-                ? splitKeywords(options.rulesDirectory)
-                : options.rulesDirectory;
-            packages = options.rulesDirectory;
-            
-            options.rulesDirectory = options.rulesDirectory.map(
+        // options
+        var rulesDirectory = this.options.rulesDirectory;
+        if (this.options.config && this.options.config.rulesDirectory) {
+            rulesDirectory = this.options.config.rulesDirectory
+        }
+        if (typeof rulesDirectory === "boolean") {
+            this.log('Maybe you forgot double dash: `-rulesDirectory` instead of `--rulesDirectory`');
+        }
+        if (rulesDirectory) {
+            cli.rulesDirectory = (typeof rulesDirectory === 'string')
+                ? splitKeywords(rulesDirectory)
+                : rulesDirectory;
+            packages = cli.rulesDirectory;
+            cli.rulesDirectory = cli.rulesDirectory.map(
                 function(rule) {
                     return dictionary[rule];
                 }
             );
         }
 
+        var ext = this.options.extends;
+        if (typeof ext === "boolean") {
+            this.log('Maybe you forgot double dash: `-extends` instead of `--extends`');
+        }
+        if (ext) {
+            cli.extends = (typeof ext === 'string')
+                ? splitKeywords(ext)
+                : ext;
+        }
+
         var existing = this.fs.exists(this.destinationPath('tslint.json'))
             ? parse(this.fs.read(this.destinationPath('tslint.json')))
             : {};
-            
+
+        var options = this.options.config && !this.options.config.rulesDirectory
+            ? this.options.config
+            : {};
         var result = R.mergeAll([existing, cli, options]);
         this.fs.write(
             this.destinationPath('tslint.json'),
--- test/test.js
+++ test/test.js
@@ -28,31 +28,86 @@ describe('generator-tslint:app', function () {
     });
 
     describe('cli', function () {
-        describe('rulesDirectory', function () {
-            it('uses one rulesDirectory from arguments', function (done) {
-                var input = { rulesDirectory: 'node_modules/tslint-microsoft-contrib' };
-                generator().withArguments(['tslint-microsoft-contrib']).on('end', function () {
-                    assert.jsonFileContent('tslint.json', input);
-                    assert.fileContent('package.json', "tslint-microsoft-contrib");
-                    done();
-                });
-            });
-            it('uses several rulesDirectory array from arguments', function (done) {
-                var input = { rulesDirectory: [
-                      'node_modules/tslint-microsoft-contrib'
-                    , 'node_modules/tslint-eslint-rules/dist/rules'
-                    , 'node_modules/codelyzer/dist/src'
-                ]};
-                generator().withArguments([
-                      'tslint-microsoft-contrib'
-                    , 'tslint-eslint-rules'
-                    , 'codelyzer'])
-                    .on('end', function () {
+        describe('withArguments()', function () {
+            describe('rulesDirectory', function () {
+                it('uses one rulesDirectory', function (done) {
+                    var input = { rulesDirectory: 'node_modules/tslint-microsoft-contrib' };
+                    generator().withArguments(['tslint-microsoft-contrib']).on('end', function () {
                         assert.jsonFileContent('tslint.json', input);
                         assert.fileContent('package.json', "tslint-microsoft-contrib");
-                        assert.fileContent('package.json', "tslint-eslint-rules");
-                        assert.fileContent('package.json', "codelyzer");
                         done();
+                    });
+                });
+                it('uses several rulesDirectory array', function (done) {
+                    var input = { rulesDirectory: [
+                        'node_modules/tslint-microsoft-contrib'
+                        , 'node_modules/tslint-eslint-rules/dist/rules'
+                        , 'node_modules/codelyzer/dist/src'
+                    ]};
+                    generator().withArguments([
+                        'tslint-microsoft-contrib'
+                        , 'tslint-eslint-rules'
+                        , 'codelyzer'])
+                        .on('end', function () {
+                            assert.jsonFileContent('tslint.json', input);
+                            assert.fileContent('package.json', "tslint-microsoft-contrib");
+                            assert.fileContent('package.json', "tslint-eslint-rules");
+                            assert.fileContent('package.json', "codelyzer");
+                            done();
+                    });
+                });
+            });
+        });
+        
+        describe('withOptions()', function () {
+            describe('rulesDirectory', function () {
+                it('uses one rulesDirectory from options', function (done) {
+                    generator().withOptions({ rulesDirectory: 'tslint-microsoft-contrib' }).on('end', function () {
+                        assert.jsonFileContent('tslint.json', { rulesDirectory: 'node_modules/tslint-microsoft-contrib' });
+                        assert.fileContent('package.json', "tslint-microsoft-contrib");
+                        done();
+                    });
+                });
+            });
+            describe('rulesDirectory', function () {
+                it('uses rulesDirectory string from options', function (done) {
+                    generator().withOptions({ rulesDirectory: 'tslint-microsoft-contrib,codelyzer' }).on('end', function () {
+                        assert.jsonFileContent('tslint.json', { rulesDirectory: ['node_modules/tslint-microsoft-contrib','node_modules/codelyzer/dist/src'] });
+                        assert.fileContent('package.json', "tslint-microsoft-contrib");
+                        done();
+                    });
+                });
+            });
+            
+            describe('extends', function () {
+                it('uses extends string with one item from options', function (done) {
+                    generator().withOptions({ extends: './tslint-config' }).on('end', function () {
+                        assert.jsonFileContent('tslint.json', { extends: ['./tslint-config'] });
+                        done();
+                    });
+                });
+
+                it('uses extends string from options', function (done) {
+                    generator().withOptions({ extends: './tslint-config,./tslint-config-2' }).on('end', function () {
+                        assert.jsonFileContent('tslint.json', { extends: ['./tslint-config', './tslint-config-2'] });
+                        done();
+                    });
+                });
+
+                it('uses extends array with one item from options', function (done) {
+                    var input = { extends: ['./tslint-config'] };
+                    generator().withOptions(input).on('end', function () {
+                        assert.jsonFileContent('tslint.json', input);
+                        done();
+                    });
+                });
+
+                it('uses extends array from options', function (done) {
+                    var input = { extends: ['./tslint-config', './tslint-config-2'] };
+                    generator().withOptions(input).on('end', function () {
+                        assert.jsonFileContent('tslint.json', input);
+                        done();
+                    });
                 });
             });
         });
@@ -70,7 +125,7 @@ describe('generator-tslint:app', function () {
             var input = { rulesDirectory: 'tslint-microsoft-contrib' };
             var output = { rulesDirectory: 'node_modules/tslint-microsoft-contrib' };
             generator().withOptions({ config: input }).on('end', function () {
-                assert.jsonFileContent('tslint.json', input);
+                assert.jsonFileContent('tslint.json', output);
                 assert.fileContent('package.json', "tslint-microsoft-contrib");
                 done();
             });
@@ -83,7 +138,7 @@ describe('generator-tslint:app', function () {
                         , 'codelyzer'
                     ]
                 };
-            var output = { 
+            var output = {
                 rulesDirectory: [
                       'node_modules/tslint-microsoft-contrib'
                     , 'node_modules/tslint-eslint-rules/dist/rules'
@@ -95,6 +150,21 @@ describe('generator-tslint:app', function () {
                 assert.fileContent('package.json', "tslint-microsoft-contrib");
                 assert.fileContent('package.json', "tslint-eslint-rules");
                 assert.fileContent('package.json', "codelyzer");
+                done();
+            });
+        });
+        
+        it('uses config.extends string option', function (done) {
+            var input = { extends: './tslint-config' };
+            generator().withOptions({ config: input }).on('end', function () {
+                assert.jsonFileContent('tslint.json', input);
+                done();
+            });
+        });
+        it('uses config.extends array option', function (done) {
+            var input = { extends: ['./tslint-config', './tslint-config-2'] };
+            generator().withOptions({ config: input }).on('end', function () {
+                assert.jsonFileContent('tslint.json', input);
                 done();
             });
         });
